12c2e757ba9da269b39c1f3554387cdfb962d98a
Sundar Khanal <sundarkhanal62@gmail.com>
2025-06-13 23:55:11 +0545

    contact form and map added

--- minor_project/src/components/Pages/Contact.jsx
+++ minor_project/src/components/Pages/Contact.jsx
@@ -21,8 +21,35 @@ const Contact = () => {
         </section>
       </div>
 
+      <section className='contact-form'>
+        <div className="form-container">
+          <form action="" className='contact-right'>
+            <div className="contact-right-title">
+              <h2>Get in Touch</h2>
+            <hr />
+            </div>
+            <input type="text" name="" id="name" placeholder='Enter your name' className='contact-inputs' /> <br />
+            <input type="email" name="" id="email" placeholder='Enter your email' className='contact-inputs'  /> <br />
+            <input type="number" name="" id="number" placeholder='Enter your number' className='contact-inputs' /> <br />
+            <button type="submit" className='btn-submit'>Submit</button>
+          </form>
+        </div>
+        <div className="map-container">
+          <h2>You can find us on Map also</h2>
+          <iframe 
+            title="Our Location on Google Maps"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.4532356003424!2d85.30638687492291!3d27.703289125671382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1941220ba5af%3A0xb61c5db192250183!2sNew%20road%20book%20store!5e0!3m2!1sen!2snp!4v1749834367145!5m2!1sen!2snp" 
+            width="100%"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade">
+            </iframe>
+          </div>
+      </section>
+</div>
 
-    </div>
     </>
   )
 }
